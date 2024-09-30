72f133896d219764adbd002990e452e60274024c
Sovxy Zero <sovxy@my.yorku.ca>
2024-09-30 18:32:24 -0400

    Updated bex3a.asm

--- LabSaveFiles/LabB/bex3a.asm
+++ LabSaveFiles/LabB/bex3a.asm
@@ -1,9 +1,9 @@
 s1: DC "What is your name?"
-s2: DC "John"
-s3: DC "Hello John!"	
+s3: DC "Hello"	
+s4: DC "Hello John"
 addi x29,x0,s1
-addi x30,x0,s2
 addi x31,x0,s3
 ecall x0, x29, 4 ;info string
-ecall x0, x30, 4 ;info string
-ecall x0, x31, 4 ;info string 
+ecall x5, x0, 8
+addi x4,x0,32
+ecall x0,x4,4
