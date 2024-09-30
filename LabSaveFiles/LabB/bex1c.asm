2219cf9e3ccd142f230d117010c6e4512bdc7847
Sovxy Zero <sovxy@my.yorku.ca>
2024-09-30 17:41:07 -0400

    Updated bex1c.asm

--- LabSaveFiles/LabB/bex1c.asm
+++ LabSaveFiles/LabB/bex1c.asm
@@ -1,13 +1,14 @@
 a2:DD 0xAAAABBBBCCCCDDDD
 b2:DD 0x4444333322221111
 
-DM 7
+DM 8
 
 ld x4,a2(x0)
 ld x5,b2(x0)
 
 add x6,x4,x5
 sub x7,x4,x5
+sub x12,x5,x4
 and x8,x4,x5
 or x9,x4,x5
 xor x10,x4,x5
@@ -16,8 +17,9 @@ xori x3,x5,-1
 
 sd x6,16(x0)
 sd x7,24(x0)
-sd x8,32(x0)
-sd x9,40(x0)
-sd x10,48(x0)
-sd x2,56(x0)
-sd x3,64(x0)
+sd x12,32(x0)
+sd x8,40(x0)
+sd x9,48(x0)
+sd x10,56(x0)
+sd x2,64(x0)
+sd x3,72(x0)
